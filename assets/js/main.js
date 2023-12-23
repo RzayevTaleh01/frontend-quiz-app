fe824c3c469497d8a77f28437aed57a1aef09a37
Taleh Rzayev <taleh.rzayev.i@asoiu.edu.az>
2023-12-24 03:30:39 +0400

    Mock Data  - CurrentUrl - Answer - Check Answer

---
+++ assets/js/main.js
@@ -0,0 +1,137 @@
+//  - Mock Data
+//  - CurrentUrl
+//  - Answer 
+//  - Check Answer
+
+let quizAnswers;
+let answersBlock = document.querySelector('.quiz-block_categories');
+
+
+
+
+let data = [
+    {
+        id: '1',
+        img: './assets/img/html.svg',
+        name: 'HTML',
+        questions: [
+            {
+                id: '1',
+                text: 'Which of these color contrast ratios defines the minimum WCAG 2.1 Level AA requirement for normal text?',
+                rightAnswerID: '3',
+                answers: [
+                    {
+                        id: '1',
+                        variant: 'A',
+                        text: '4.5 : 1'
+                    },
+                    {
+                        id: '2',
+                        variant: 'B',
+                        text: '1.5 : 1'
+                    },
+                    {
+                        id: '3',
+                        variant: 'C',
+                        text: '2.5 : 2'
+                    },
+                    {
+                        id: '4',
+                        variant: 'D',
+                        text: '3.5 : 1'
+                    }
+
+                ]
+
+            },
+            {
+                id: '2',
+                text: 'Which of these color contrast ratios defines the minimum WCAG 2.1 Level AA requirement for normal text?',
+                rightAnswerID: '3',
+                answers: [
+                    {
+                        id: '1',
+                        variant: 'A',
+                        text: '4.5 : 1',
+                    },
+                    {
+                        id: '2',
+                        variant: 'B',
+                        text: '1.5 : 1'
+                    },
+                    {
+                        id: '3',
+                        variant: 'C',
+                        text: '2.5 : 2'
+                    },
+                    {
+                        id: '4',
+                        variant: 'D',
+                        text: '3.5 : 1'
+                    }
+
+                ]
+
+            }
+        ]
+    },
+    {
+        id: '2',
+        img: './assets/img/css.svg',
+        name: 'Css'
+    }
+    ,
+    {
+        id: '3',
+        img: './assets/img/js.svg',
+        name: 'Javascript'
+    }
+    ,
+    {
+        id: '4',
+        img: './assets/img/accessibility.svg',
+        name: 'Accessibility'
+    }
+
+]
+
+function handleAnswerClick(event) {
+    quizAnswers.forEach((item) => {
+        item.classList.remove('selected')
+    })
+
+    event.target.classList.add('selected');
+}
+
+
+
+function getData(a) {
+    let dataHtml = '';
+
+    a.map((item) => {
+        dataHtml += `
+        <li data-id='${item.id}' class="quiz-block_li">
+                            <div class="quiz-block_li--variant">
+                                <p>${item.variant}</p>
+                            </div>
+                            <p class="quiz-block_li--title">
+                                ${item.text}
+                            </p>
+                        </li>
+        `
+    })
+
+    answersBlock.innerHTML = dataHtml;
+    
+    quizAnswers = document.querySelectorAll('.quiz-block_li');
+    quizAnswers.forEach(item => {
+        item.addEventListener('click', handleAnswerClick)
+    })
+
+}
+
+
+
+
+getData(data[0].questions[0].answers)
+
